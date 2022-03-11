09e1ee74f0588fb6d004b2316a737df15d0e66e3
NE5054 <w_lan123@naver.com>
2022-03-11 22:05:53 +0900

    chore: eslint prettier airbnb package install & eslint prettier 설정

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/essential',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: ['vue', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'es5',
+        tabWidth: 2,
+        semi: true,
+        singleQuote: true,
+        quoteProps: 'consistent',
+        bracketSpacing: true,
+        arrowParens: 'always',
+        endOfLine: 'auto',
+        printWidth: 120,
+      },
+    ],
+    'no-console': 'off',
+    'no-plusplus': 'off',
+    'max-depth': ['error', 2],
+    'max-lines-per-function': 'off', // ['error', 15],
+    'no-restricted-syntax': 'off',
+    'no-continue': 'off',
+    'no-alert': 'off',
+    'max-len': 'off',
+    'import/extensions': 'off',
+    'no-return-assign': 'off',
+    'linebreak-style': 0,
+    'import/no-unresolved': 'off', // ['error', { caseSensitive: false }],
+    'no-param-reassign': 'off',
+    'no-undef': 'off',
+  },
+};
