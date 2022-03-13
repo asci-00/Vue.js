aa5d59d069f12a22e77ac554710a109377d51b07
NE5054 <w_lan123@naver.com>
2022-03-13 20:12:19 +0900

    feat: 할 일 목록 관리 (출력 / 삭제 / 체크 / 초기화) 기능

---
+++ src/components/TodoList.vue
@@ -0,0 +1,95 @@
+<template>
+  <section class="wrapper-item-list">
+    <ul class="item-list">
+      <li v-for="(item, index) in itemList" :key="item.id">
+        <span>
+          <i :class="`fa-solid fa-circle-check icon ${item.check && 'checked'}`" @click="itemClick(index)"></i>
+          {{ item.text }}
+        </span>
+        <i class="far fa-trash-alt icon delete-btn" @click="removeItem(index)"></i>
+      </li>
+    </ul>
+    <button class="clear-btn" @click="clearItemList">Clear</button>
+  </section>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  data() {
+    return {
+      itemList: [
+        {
+          id: -1,
+          text: 'Some Schedule',
+          check: false,
+        },
+      ],
+    };
+  },
+  created() {
+    this.emitter.on('onItemAdd', (item) => {
+      this.itemList.push(item);
+    });
+  },
+  methods: {
+    itemClick(index) {
+      this.itemList[index].check = !this.itemList[index].check;
+    },
+    removeItem(index) {
+      this.itemList.splice(index, 1);
+    },
+    clearItemList() {
+      this.itemList = [];
+    },
+  },
+});
+</script>
+
+<style scoped>
+.wrapper-item-list {
+  text-align: center;
+}
+.item-list {
+  list-style: none;
+  box-sizing: border-box;
+  width: 700px;
+  padding: 20px;
+  margin: 10px auto;
+}
+.item-list li {
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: #fff;
+  padding: 10px;
+  border-radius: 2px;
+  margin-bottom: 10px;
+  border: 1px solid grey;
+}
+
+.item-list li:last-of-type {
+  margin-bottom: 0;
+}
+
+.icon {
+  cursor: pointer;
+  width: 20px;
+  height: 20px;
+}
+.checked {
+  color: greenyellow;
+}
+.delete-btn {
+  color: red;
+}
+.clear-btn {
+  width: 100px;
+  padding: 10px;
+  color: #cc3300;
+  border-radius: 5px;
+  border: 1px solid grey;
+}
+</style>
