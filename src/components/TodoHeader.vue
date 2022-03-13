2b73908129e21617ebc7df86aca13b9b21549567
NE5054 <w_lan123@naver.com>
2022-03-13 18:52:19 +0900

    feat: Header styling

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <h1>Todo Application</h1>
+  </header>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({});
+</script>
+
+<style scoped>
+h1 {
+  color: #2f3b52;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
+  text-transform: uppercase;
+}
+</style>
