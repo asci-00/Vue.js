252d4a6e08347b78e7c4944b7d680d08b816316e
asci-00 <w_lan123@naver.com>
2022-04-11 23:15:00 +0900

    feat: UserView 구현 및 불필요 console.log 제거

--- vue-practice/src/views/User.vue
+++ vue-practice/src/views/User.vue
@@ -1,9 +1,39 @@
 <template>
-  <div></div>
+  <div id="info-container">
+    <p>name: {{ userInfo.id }}</p>
+    <p>karma: {{ userInfo.karma }}</p>
+    <p>created: {{ userInfo.created }}</p>
+  </div>
 </template>
 
-<script>
-export default {};
+<script lang="ts">
+import { defineComponent, computed } from 'vue';
+import { useRoute } from 'vue-router';
+import { useStore } from 'vuex';
+
+export default defineComponent({
+  setup() {
+    const route = useRoute();
+    const store = useStore();
+
+    const { name } = route.params;
+    store.dispatch('user/FETCH_USER', name);
+
+    const userInfo = computed(() => store.state.user.data);
+    return { userInfo };
+  },
+});
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+#info-container {
+  position: relative;
+  width: 500px;
+  height: 600px;
+  border-radius: 5px;
+  background: #eee;
+  box-shadow: 2px 7px 7px 3px #888888;
+  padding: 20px;
+  margin: 0 auto;
+}
+</style>
